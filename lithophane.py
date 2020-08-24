0b8bc66776b745251c89c84ebd4e6f937bad6a68
huykanit <calemolech@gmail.com>
2020-08-24 17:19:24 +0700

    Update shape, render flow and button control

--- lithophane.py
+++ lithophane.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from stl import mesh
 
+
 class ImageMap:
     def __init__(self, image_path, positive=True, mirror=False, flip=False):
         self.image = cv2.imread(image_path)
@@ -10,7 +11,6 @@ class ImageMap:
         self.mirror = mirror
         self.flip = flip
 
-
     def process_image(self):
         return 1
 
@@ -18,7 +18,7 @@ class ImageMap:
 
     # TODO: 2. Scale image
     @staticmethod
-    def scale_image(image, width_mm=40):
+    def scale_image(image, width_mm=400):
         """Scale image to 0.1 pixel width
 
         Will make an image with 1000 pixels wide.
@@ -26,7 +26,7 @@ class ImageMap:
         """
         height = image.shape[0]
         width = image.shape[1]
-        scale = (width_mm * 10 / width)
+        scale = (width_mm / width)
         new_size = (int(width * scale), int(height * scale))
         output = cv2.resize(image, new_size, interpolation=cv2.INTER_CUBIC)
         return output
@@ -47,7 +47,7 @@ class ImageMap:
         return self
 
     # TODO: 5. Convert2D image to CloudPoint
-    def image2points(self, width='', max_thickness=3.0, min_thickness=0.5, show=True):
+    def image2points(self, width='', max_thickness=3.0, min_thickness=0.5):
         depth = max_thickness
         offset = min_thickness
 
@@ -66,11 +66,11 @@ class ImageMap:
         self.gray = self.gray / 255.0
 
         # g = np.fliplr(g)
-        if (show):
-            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-            cv2.imshow('image', self.gray)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        # if (show):
+        #     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        #     cv2.imshow('image', self.gray)
+        #     cv2.waitKey(0)
+        #     cv2.destroyAllWindows()
 
         # print(np.max(g))
         # print(g.shape)
@@ -79,7 +79,7 @@ class ImageMap:
         ngray = 1 - np.double(self.gray)
 
         # scale z matrix to desired max depth and add base height
-        z_middle = ngray * depth + offset
+        z_middle = ngray * (depth - offset) + offset
 
         # add border of zeros to help with back.
         z = np.zeros([z_middle.shape[0] + 2, z_middle.shape[1] + 2])
@@ -90,13 +90,9 @@ class ImageMap:
 
         x, y = np.meshgrid(x1, y1)
 
-        x = np.fliplr(x)
+        # x = np.fliplr(x)
+        y = np.flipud(y)
         return x, y, z
-
-    @staticmethod
-    def testmesh(x,y,z):
-        point_cloud = np.dstack((x, y, x))
-        return point_cloud
 
     @staticmethod
     def makemesh(x, y, z):
@@ -104,6 +100,8 @@ class ImageMap:
         count = 0
         points = []
         triangles = []
+        import time
+        t1 = time.time()
         for i in range(z.shape[0] - 1):
             for j in range(z.shape[1] - 1):
                 # Triangle 1
@@ -123,6 +121,7 @@ class ImageMap:
                 count += 6
 
         # BACK
+        t2 = time.time()
         for j in range(x.shape[1] - 1):
             bot = x.shape[0] - 1
 
@@ -143,35 +142,114 @@ class ImageMap:
             count += 6
 
         # Create the mesh
+        t3 = time.time()
         model = mesh.Mesh(np.zeros(len(triangles), dtype=mesh.Mesh.dtype))
         for i, f in enumerate(triangles):
             for j in range(3):
                 model.vectors[i][j] = points[f[j]]
+        t4 = time.time()
+
+        print(t2 - t1, t3 - t2, t4 - t3, t4 - t1)
 
         return model
 
+    # @staticmethod
+    # def point_cloud(depth):
+    #     """Transform a depth image into a point cloud with one point for each
+    #     pixel in the image, using the camera transform for a camera
+    #     centred at cx, cy with field of view fx, fy.
+    #
+    #     depth is a 2-D ndarray with shape (rows, cols) containing
+    #     depths from 1 to 254 inclusive. The result is a 3-D array with
+    #     shape (rows, cols, 3). Pixels with invalid depth in the input have
+    #     NaN for the z-coordinate in the result.
+    #
+    #     """
+    #     rows, cols = depth.shape
+    #     c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+    #     valid = (depth > 0) & (depth < 255)
+    #     z = np.where(valid, depth / 256.0, np.nan)
+    #     x = np.where(valid, z * (c - self.cx) / self.fx, 0)
+    #     y = np.where(valid, z * (r - self.cy) / self.fy, 0)
+    #     # return np.dstack((x, y, z))
+    #     return x,y,z
+
     @staticmethod
-    def point_cloud(self, depth):
-        """Transform a depth image into a point cloud with one point for each
-        pixel in the image, using the camera transform for a camera
-        centred at cx, cy with field of view fx, fy.
+    def makeCylinder(x, y, z):
+        '''Convert flat point cloud to Cylinder'''
+        newx = x.copy()
+        newz = z.copy()
+        radius = (np.max(x) - np.min(x)) / (2 * np.pi)
+        print(f"Cylinder Radius {radius}mm")
+        for r in range(0, x.shape[0]):
+            for c in range(0, x.shape[1]):
+                t = (c / (x.shape[1] - 10)) * 2 * np.pi
+                rad = radius + z[r, c]
+                newx[r, c] = rad * np.cos(t)
+                newz[r, c] = rad * np.sin(t)
+        return newx, y.copy(), newz
 
-        depth is a 2-D ndarray with shape (rows, cols) containing
-        depths from 1 to 254 inclusive. The result is a 3-D array with
-        shape (rows, cols, 3). Pixels with invalid depth in the input have
-        NaN for the z-coordinate in the result.
+    @staticmethod
+    def makeCylinder2(x, y, z, curve, shape):
+        newx = x.copy()
+        newz = z.copy()
 
-        """
-        rows, cols = depth.shape
-        c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
-        valid = (depth > 0) & (depth < 255)
-        z = np.where(valid, depth / 256.0, np.nan)
-        x = np.where(valid, z * (c - self.cx) / self.fx, 0)
-        y = np.where(valid, z * (r - self.cy) / self.fy, 0)
-        return np.dstack((x, y, z))
+        # If type = cylinder , overlap offset =1
+        if shape == 'Cylinder':
+            overlap_offset = 1
+        else:
+            overlap_offset = 0
+
+        width = x.shape[1]
+        height = x.shape[0]
+
+        if curve == 0:
+            return x,y,z
+        if curve != 0:
+            deg2Rad = (np.pi / 180)
+            angle = abs(curve)
+            arcRadius = (width / curve) * (180 / np.pi)
+            yarcRadius = (height / curve) * (180 / np.pi)
+            distanceFromFlat = np.sin(angle * (360 / np.pi)) * arcRadius
+            if (angle >= 180):
+                distanceFromFlat = 0
+            startAngle = (0 - angle / 2)
+
+            if (curve < 0):
+                distanceFromFlat = 0 - distanceFromFlat
+
+        for i in range(0, height):
+            for j in range(0, width):
+                jpos = j
+                ipos = i
+
+                if j == 1:
+                    jpos -= 1
+                elif j == (width - 1):
+                    jpos += 1
+                if i == 1:
+                    ipos -= 1
+                elif i == (height - 1):
+                    ipos += 1
+
+
+                degreesRotated = startAngle + (jpos / (width - overlap_offset) * angle)
+                rotation = degreesRotated * deg2Rad
+                magnitude = arcRadius + z[i, j]
+
+                # Cylinder, Curve
+                if (shape in ['Cylinder', 'Curver']):
+                    newx[i, j] = width / 2 + magnitude * np.cos(rotation)
+                    newz[i, j] = distanceFromFlat + magnitude * np.sin(rotation)
+
+                # Heart
+                if shape == 'Heart':
+                    newx[i, j] = width / 2 + magnitude * ((16 * np.sin(rotation) * np.sin(rotation) * np.sin(rotation)) / 16);
+                    newz[i, j] = distanceFromFlat + magnitude * ((13 * np.cos(rotation) - 5 * np.cos(2 * rotation) - 2 * np.cos(3 * rotation) - np.cos(4 * rotation)) / 16);
+
+        return newz, y.copy(), newx
+
 
 class Lithophane:
     def __index__(self):
         self.thickness = 1
-
-
--- main.py
+++ main.py
@@ -1,39 +1,28 @@
-import sys
-import cv2
-from PyQt5 import Qt
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QGraphicsPixmapItem, QFileDialog
-
-from matplotlib import cm
-from matplotlib.pyplot import figure
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from mpl_toolkits.mplot3d import Axes3D
-
-from resources.ui.main_window import *
-from lithophane import *
-from stl import mesh
-import numpy as np
 import pyvista as pv
-from pyvistaqt import BackgroundPlotter, QtInteractor
-from pyvista import examples
-
-
-class Config():
-    def __init__(self):
-        shape = 'Flat'
-        size = 800
-        max_thickness = 3.5
-        min_thickness = 0.5
-        border = False
-        border_thickness = 0
+from PyQt5 import Qt, QtGui
+from PyQt5.QtWidgets import QMainWindow, QGraphicsScene, QFileDialog, \
+    QGraphicsPixmapItem
+from pyvistaqt import QtInteractor
+import sys
+from config import Config
+from lithophane import *
+from resources.ui.main_window import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = UiMainWindow()
         self.ui.setupUi(self)
+
+        # Config menubar
+        self.ui.actionOpen.triggered.connect(self.open_file)
+        self.ui.actionExit.triggered.connect(app.quit)
+
+        # Config trigger function
+        self.ui.shapeComboBox.currentTextChanged.connect(
+            self.update_config_by_shape)
+
         self.ui.shapeComboBox.addItems(["Flat", "Cylinder", "Curve", "Heart"])
 
         self.config = Config()
@@ -43,7 +32,7 @@ class MainWindow(QMainWindow):
         self.output_scene = QGraphicsScene(self)
 
         # Open File
-        self.ui.selectImageButton.clicked.connect(self.openFile)
+        self.ui.selectImageButton.clicked.connect(self.open_file)
 
         # Render test
         self.ui.renderButton.clicked.connect(self.render_file)
@@ -52,68 +41,83 @@ class MainWindow(QMainWindow):
         self.plotter = QtInteractor(self.frame)
         self.ui.outputLayout.addWidget(self.plotter.interactor)
 
-    def openFile(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getOpenFileName(self, "Open Image", QtCore.QDir.currentPath(),
-                                                   "Images (*.png *.jpg)", options=options)
+    def init_new_plotter(self):
+        self.ui.outputLayout.removeWidget(self.plotter.interactor)
+        self.plotter = QtInteractor(self.frame)
+        self.ui.outputLayout.addWidget(self.plotter.interactor)
+        # self.plotter.show_grid()
+        # self.plotter.enable_zoom_style()
+        self.plotter.show_axes_all()
+        # self.plotter.view_xy()
+
+    def open_file(self):
+        file_name,_ = QFileDialog.getOpenFileName(self, "Open Image",
+                                                   QtCore.QDir.currentPath(),
+                                                   "Images (*.png *.jpg)")
         # file_name = "/Users/cale/Desktop/M1-IEI/Project/04_Code/lithophanes/a.jpg"
         if file_name:
             # Qt Graphics
-            # self.ui.lineImagePath.setText(file_name)
-            # self.input_scene.clear()
-            # pixmap = QtGui.QPixmap(file_name)
-            # pixmap = pixmap.scaled(0.95 * self.ui.graphicsViewInput.size(), QtCore.Qt.KeepAspectRatio)
-            # item = QGraphicsPixmapItem(pixmap)
-            # self.input_scene.addItem(item)
-            # self.ui.graphicsViewInput.setScene(self.input_scene)
+            self.ui.lineImagePath.setText(file_name)
+            self.input_scene.clear()
+            pixmap = QtGui.QPixmap(file_name)
+            pixmap = pixmap.scaled(0.95 * self.ui.graphicsViewInput.size(), QtCore.Qt.KeepAspectRatio)
+            item = QGraphicsPixmapItem(pixmap)
+            self.input_scene.addItem(item)
+            self.ui.graphicsViewInput.setScene(self.input_scene)
 
-
-            image2d = ImageMap(file_name)
-            image = image2d.image
-            config = self.get_config()
-            # x,y,z = image2d.image2points(width=40, max_thickness=config.max_thickness, min_thickness=config.min_thickness, show=False)
-            x,y,z = image2d.image2points(width=40, max_thickness=20, min_thickness=20, show=False)
-
-            # points = image2d.testmesh(x,y,z)
-            # print(points.shape)
-
-
-
-            model = image2d.makemesh(x, y,z )
-            model.save("temp.stl")
-            print("ok")
-            sphere = pv.read("temp.stl")
-            self.plotter.show_grid()
-            self.plotter.show_bounds()
-            self.plotter.show_axes()
-
-            self.plotter.add_mesh(sphere)
-
-            # grid = pv.StructuredGrid(x, y, z)
-
-            # self.plotter.add_mesh(grid)
+            self.image2d = ImageMap(file_name)
 
     def render_file(self):
-        config = self.get_config();
+        self.config = self.get_ui_config()
 
-        sphere = pv.read("/Users/cale/Desktop/M1-IEI/Project/04_Code/lithophanes/out_stl/c.stl")
-        sphere = pv.read("/Users/cale/Desktop/M1-IEI/Project/04_Code/lithophanes/out_stl/a_Cylinder.stl")
-        sphere = pv.read("/Users/cale/Desktop/M1-IEI/Project/04_Code/lithophanes/out_stl/a_Flat.stl")
+        x, y, z = self.image2d.image2points(self.config.size,
+                                            self.config.max_thickness,
+                                            self.config.min_thickness)
+        # x2,y2,z2 = image2d.makeCylinder(x, y, z)
+        x2, y2, z2 = self.image2d.makeCylinder2(x, y, z,
+                                                self.ui.curveSpinBox.value(),
+                                                self.ui.shapeComboBox.currentText())
+        model = self.image2d.makemesh(x2, y2, z2)
+        model.save("temp.stl")
+        print("ok")
+        sphere = pv.read("temp.stl")
+        sphere = pv.StructuredGrid(x2, y2, z2)
+
+
+
+        self.init_new_plotter()
         self.plotter.add_mesh(sphere)
+        self.plotter.camera_position = [(0, 0, z.shape[1] * 3.5),
+                                        sphere.center, (0, 1, 0)]
 
-    def get_config(self):
-        self.config.shape = self.ui.shapeComboBox.currentData(self.ui.shapeComboBox.currentIndex())
-        self.config.size = self.ui.sizeSlider.value()
-        self.config.border = True if self.ui.borderYesRadioButton.isChecked() else False
-        self.config.max_thickness = self.ui.maxThickDoubleSpinBox.value()
-        self.config.min_thickness = self.ui.minThickDoubleSpinBox.value()
-        self.config.border_thickness = self.ui.borderThicknessSlider.value()
-        return self.config
+    def get_ui_config(self):
+        shape_index = self.ui.shapeComboBox.currentIndex()
+        use_border = True if self.ui.borderYesRadioButton.isChecked() else False
 
+        current_config = Config(
+            shape=self.ui.shapeComboBox.currentData(shape_index),
+            size=self.ui.sizeSlider.value(),
+            min_thick=self.ui.minThickDoubleSpinBox.value(),
+            max_thick=self.ui.maxThickDoubleSpinBox.value(),
+            curve=self.ui.curveSpinBox.value(),
+            use_border=use_border,
+            border_thick=self.ui.borderThicknessSlider.value()
+        )
+        return current_config
+
+    def update_config_by_shape(self):
+        shape = self.ui.shapeComboBox.currentText()
+        if shape == 'Heart':
+            self.ui.curveSpinBox.setValue(360)
+            self.ui.curveSpinBox.setDisabled(True)
+            self.ui.curveSlider.setDisabled(True)
+        elif shape == 'Cylinder':
+            self.ui.curveSpinBox.setValue(360)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationDisplayName("3D Lithophanes")
+    # app.setStyle("Fusion")
     TestMainWindow = MainWindow()
     TestMainWindow.show()
     sys.exit(app.exec_())
